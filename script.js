77022a353e24b57c595b89d2e74f52cdc4089ac6
Nitin shukla <nitin.shukla12004@gmail.com>
2024-06-29 10:27:47 +0530

    Used gsap for the first time and finished the project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.addEventListener("mousemove",function(details){
+    let val = gsap.utils.mapRange(0,window.innerWidth,100,window.innerWidth-100,details.clientX)
     gsap.to("#con",{
-        left: details.clientX+'px',
+        left: val+'px',
         ease: Power3
     })
 })
